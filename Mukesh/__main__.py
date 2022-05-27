993b165a1d26101ac2d3c8144bc8b27118981d30
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-27 16:02:04 +0530

    Update __main__.py

--- Mukesh/__main__.py
+++ Mukesh/__main__.py
@@ -21,19 +21,19 @@ from youtubesearchpython import VideosSearch
 
 from config import (LOG_GROUP_ID, LOG_SESSION, STRING1, STRING2, STRING3,
                     STRING4, STRING5)
-from Alexa import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
+from Mukesh import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
                    ASSID1, ASSID2, ASSID3, ASSID4, ASSID5, ASSNAME1, ASSNAME2,
                    ASSNAME3, ASSNAME4, ASSNAME5, BOT_ID, BOT_NAME, LOG_CLIENT,
                    OWNER_ID, app)
-from Alexa.Core.Clients.cli import LOG_CLIENT
-from Alexa.Core.PyTgCalls.Alexa import (pytgcalls1, pytgcalls2, pytgcalls3,
+from Mukesh.Core.Clients.cli import LOG_CLIENT
+from Mukesh.Core.PyTgCalls.Alexa import (pytgcalls1, pytgcalls2, pytgcalls3,
                                         pytgcalls4, pytgcalls5)
-from Alexa.Database import (get_active_chats, get_active_video_chats,
+from Mukesh.Database import (get_active_chats, get_active_video_chats,
                             get_sudoers, is_on_off, remove_active_chat,
                             remove_active_video_chat)
-from Alexa.Inline import private_panel
-from Alexa.Plugins import ALL_MODULES
-from Alexa.Utilities.inline import paginate_modules
+from Mukesh.Inline import private_panel
+from Mukesh.Plugins import ALL_MODULES
+from Mukesh.Utilities.inline import paginate_modules
 
 loop = asyncio.get_event_loop()
 console = Console()
